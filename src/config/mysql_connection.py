32c891c7f024c2b097d1a68f59b6cffb7dbdf79f
JOSSEyela <jotay234@gmail.com>
2026-09-10 21:26:48 -0500

    primer commit

---
+++ src/config/mysql_connection.py
@@ -0,0 +1,18 @@
+import os
+
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_mysql_connection():
+  return mysql.connector.connect(
+    host=os.getenv('MYSQL_HOST', 'localhost'),
+    port=int(os.getenv('MYSQL_PORT', '3306')),
+    user=os.getenv('MYSQL_USER', 'root'),
+    password=os.getenv('MYSQL_PASSWORD', '123456'),
+    database=os.getenv('MYSQL_DATABASE', 'mercancia'),
+    charset='utf8mb4',
+    connection_timeout=5,
+  )
